c11f0a3d0d1590c588b0e96d18e4fac7d538471e
Juwita Natalia Sinaga <juwitanatalia21@gmail.com>
2024-03-16 00:47:43 +0700

    [FIX] fix minor bug

--- src/main/java/app/readingtracker/repository/BookRepository.java
+++ src/main/java/app/readingtracker/repository/BookRepository.java
@@ -14,6 +14,6 @@ public interface BookRepository extends JpaRepository<Book, String> {
     @Query(value = "SELECT * FROM book b WHERE b.title LIKE %?1% OR b.author LIKE %?1%", nativeQuery = true)
     List<Book> findByAuthorOrTitleLike(String title);
 
-    @Query(value = "SELECT title, author, description FROM book b JOIN book_list l ON b.id = l.book_id WHERE l.id = ?1", nativeQuery = true)
+    @Query(value = "SELECT b.title, b.author, b.description FROM book b JOIN book_list l ON b.id = l.book_id WHERE l.id = ?1", nativeQuery = true)
     List<BookShortDetailResponse> getShortDescription(String listId);
 }
